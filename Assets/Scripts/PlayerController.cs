ac227b1c1d96ad40cfdc0bcb59bc75b7a02be695
Jean Robert <jradesenv@gmail.com>
2017-01-12 23:25:18 -0200

    aula 2 done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+
+        if (horizontalAxis != 0.0f) //left & right
+        {
+            transform.Translate(new Vector3(horizontalAxis * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        if (verticalAxis != 0.0f) //up & down
+        {
+            transform.Translate(new Vector3(0f, verticalAxis * moveSpeed * Time.deltaTime, 0f));
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+** Tutorial **  
+https://www.youtube.com/playlist?list=PLiyfvmtjWC_X6e0EYLPczO9tNCkm2dzkm
+
