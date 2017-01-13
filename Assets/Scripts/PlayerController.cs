9a1e516d86742d6e5787954d7d9d21c395ab51df
Jean Robert <jradesenv@gmail.com>
2017-01-13 00:34:30 -0200

    aula 4 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed;
 
     private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
@@ -18,17 +20,34 @@ public class PlayerController : MonoBehaviour {
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
 
+        playerMoving = false;
+
         if (horizontalAxis != 0.0f) //left & right
         {
             transform.Translate(new Vector3(horizontalAxis * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
         }
 
         if (verticalAxis != 0.0f) //up & down
         {
             transform.Translate(new Vector3(0f, verticalAxis * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
         }
 
+        if (playerMoving)
+        {
+            lastMove = new Vector2(horizontalAxis, verticalAxis);
+        }
+
+        SetAnimatorProperties(horizontalAxis, verticalAxis);
+    }
+
+    private void SetAnimatorProperties(float horizontalAxis, float verticalAxis)
+    {
         anim.SetFloat("MoveX", horizontalAxis);
         anim.SetFloat("MoveY", verticalAxis);
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
