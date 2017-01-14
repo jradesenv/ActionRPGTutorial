b52998daae4d6e44d433a02069e41e8b5750730b
Jean Robert <jradesenv@gmail.com>
2017-01-14 01:28:26 -0200

    aula 5 done

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    private Vector3 targetPosition;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+	}
+}
