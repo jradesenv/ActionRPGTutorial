e6ce3ac3541d4a87c673b2c1b3f489650d2477e5
Jean Robert <jradesenv@gmail.com>
2017-01-15 14:22:22 -0200

    aula 12 done - player killing & enemy movement

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SlimeController : MonoBehaviour {
 
     public float moveSpeed;
     public float timeBetweenMove;
     public float timeToMove;
+    public float waitToReload;
 
     private float timeBetweenMoveCounter;
     private float timeToMoveCounter;
     private Rigidbody2D myRigidBody;
     private bool isMoving;
     private Vector3 moveDirection;
+    private bool isReloading;
+    private GameObject thePlayer;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
 
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
-	}
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.75f);
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.75f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,7 +39,7 @@ public class SlimeController : MonoBehaviour {
             if(timeToMoveCounter < 0)
             {
                 isMoving = false;
-                timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.75f);
             }
         } else
         {
@@ -42,9 +49,29 @@ public class SlimeController : MonoBehaviour {
             if(timeBetweenMoveCounter < 0)
             {
                 isMoving = true;
-                timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.75f);
                 moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
             }
         }
+
+        if(isReloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                thePlayer.SetActive(true);
+            }
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            collision.gameObject.SetActive(false);
+            isReloading = true;
+            thePlayer = collision.gameObject;
+        }
+    }
 }
